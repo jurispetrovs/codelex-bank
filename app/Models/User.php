2b493f95442f3a1c0e6d357f3b6045854ad3f39d
Juris Petrovs <jura-petrov@inbox.lv>
2020-12-11 17:37:22 +0200

    added relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,22 +18,13 @@ class User extends Authenticatable
     use Notifiable;
     use TwoFactorAuthenticatable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name',
+        'surname',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password',
         'remember_token',
@@ -41,21 +32,21 @@ class User extends Authenticatable
         'two_factor_secret',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
 
-    /**
-     * The accessors to append to the model's array form.
-     *
-     * @var array
-     */
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function accounts()
+    {
+        return $this->hasMany(Account::class, 'holder_id', 'id');
+    }
+
+    public function getNameAndSurname()
+    {
+        return $this->name . ' ' . $this->surname;
+    }
 }
