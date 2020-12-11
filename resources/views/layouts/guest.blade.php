2268de336668163148992ecf7e330f79b14e5b46
Juris Petrovs <jura-petrov@inbox.lv>
2020-12-11 17:51:51 +0200

    changed guest navigation view

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,9 +16,40 @@
         <!-- Scripts -->
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.js" defer></script>
     </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+
+            <!-- Page Heading -->
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <div class="flex justify-between h-8">
+                        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                            <a href="{{ url('/') }}">Codelex Bank</a>
+                        </h2>
+                        <div>
+                            @if (Route::has('login'))
+                                <div >
+                                    @auth
+                                        <a href="{{ url('/overview') }}" class="text-sm text-gray-700 underline">Personal Area</a>
+                                    @else
+                                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                                        @if (Route::has('register'))
+                                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Become a customer</a>
+                                        @endif
+                                    @endauth
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </header>
+
+            <main>
+                <div class="py-12">
+                    {{ $slot }}
+                </div>
+            </main>
         </div>
     </body>
 </html>
