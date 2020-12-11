3b2bb073cffcb5a5bf4fc47fd4c2303892f55348
Juris Petrovs <jura-petrov@inbox.lv>
2020-12-11 17:54:14 +0200

    added registration and transactions events and listeners

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Events\TransactionWasCreated;
+use App\Listeners\Registered\SendWelcomeEmail;
+use App\Listeners\TransactionWasCreated\SendEmailToRecipient;
+use App\Listeners\TransactionWasCreated\SendEmailToSender;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -16,7 +20,11 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         Registered::class => [
-            SendEmailVerificationNotification::class,
+            SendWelcomeEmail::class,
+        ],
+        TransactionWasCreated::class => [
+            SendEmailToSender::class,
+            SendEmailToRecipient::class,
         ],
     ];
 
